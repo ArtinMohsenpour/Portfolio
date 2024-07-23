e852088880c4eee701060fb1a7f739709828e85c
artinmohsenpour <mohsenpour.artin@gmail.com>
2024-07-23 17:33:36 +0200

    update

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -17,7 +17,9 @@ const Work = () => {
     const query = '*[_type == "works"]';
 
     client.fetch(query).then((data) => {
-      const array1 = data.filter((work) => work.tags.includes("React"));
+      const array1 = data.filter((work) =>
+        work.tags.includes("E-commerce" || "React")
+      );
       const array2 = [...array1, ...data].filter(
         (item, index, self) => self.indexOf(item) === index
       );
