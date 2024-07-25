709a5cb440925916b792063b9da45ae85b6a6a22
artinmohsenpour <mohsenpour.artin@gmail.com>
2024-07-25 18:19:52 +0200

    moving function

--- src/components/SkillsRows.jsx
+++ src/components/SkillsRows.jsx
@@ -14,7 +14,8 @@ const SkillsRows = ({ skills, ...props }) => {
   console.log("msPerPixel: ", msPerPixel);
 
   const [rowHeight, setRowHeight] = useState(0);
-  const duration = `${rowHeight * msPerPixel}ms`;
+  //const duration = `${rowHeight * msPerPixel}ms`;
+  const duration = `$200ms`;
 
   useEffect(() => {
     if (!rowRef.current) return;
@@ -30,30 +31,27 @@ const SkillsRows = ({ skills, ...props }) => {
     };
   }, []);
 
+  const skills1 = skills.slice(0, 7);
+  const skills2 = skills.slice(7);
+  // console.log(skills1);
+  // console.log(skills2);
+
   return (
-    <div ref={rowRef} className="app__skills-list">
+    <div
+      ref={rowRef}
+      className="app__skills-list animate_1 animate-marquee"
+      style={{ "--marquee-duration": duration }}
+    >
       {skills.map((skill) => (
-        <div className="app__skills-item app__flex ">
-          <div
-            className="app__flex shadow__2"
-            style={{ backgroundColor: skill.bgColor }}
-          >
-            <img
-              key={skill.name + `${Math.random() * 100}`}
-              src={urlFor(skill.icon)}
-              alt={skill.name}
-            />
-          </div>
-          <p key={skill.name + `${Math.random() * 100}`} className="p-text">
-            {skill.name}
-          </p>
-        </div>
+        <Skill skill={skill} />
       ))}
+      
     </div>
   );
 };
 
-const skill = () => {
+
+const Skill = ({ skill, ...props }) => {
   const POSSIBLE_ANIMATION_DELAYS = [
     "0s",
     "0.1s",
@@ -67,7 +65,25 @@ const skill = () => {
     POSSIBLE_ANIMATION_DELAYS[
       Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
     ];
-  console.log(animationDelay);
+  //console.log(animationDelay);
+
+  return (
+    <div
+      className="app__skills-item app__flex animate-fade-in"
+      style={{ animationDelay }}
+    >
+      <div
+        className="app__flex shadow__2 "
+        style={{ backgroundColor: skill.bgColor }}
+        key={skill.name + `${Math.random() * 100}`}
+      >
+        <img src={urlFor(skill.icon)} alt={skill.name} />
+      </div>
+      <p key={skill.name + `${Math.random() * 100}`} className="p-text">
+        {skill.name}
+      </p>
+    </div>
+  );
 };
 
 export default SkillsRows;
