3515de4c8acee983dc5d18009b4fd71ad7774224
artinmohsenpour <mohsenpour.artin@gmail.com>
2024-08-03 22:46:26 +0200

    glowing effect

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -7,6 +7,14 @@ import { urlFor, client } from "../../client";
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
+  const [hovered, setHovered] = useState(null);
+
+  const handleMouseEnter = (key) => {
+    setHovered(key);
+  };
+  const handleMouseLeave = () => {
+    setHovered(null);
+  };
 
   useEffect(() => {
     const query = '*[_type == "abouts"]';
@@ -27,27 +35,33 @@ const About = () => {
       </div>
       <div className="wrapper__scroll">
         <div className="app__profiles box__container">
-          {abouts.map((about, index) => (
-            <motion.div
-              whileInView={{ opacity: 1 }}
-              whileHover={{ scale: 1.1 }}
-              transition={{ duration: 0.5, type: "tween" }}
-              className="app__profile-item box__container "
-              key={about.title + index}
-            >
-              <img
-                src={urlFor(about.imgUrl)}
-                alt={about.title}
-                className="shadow__1"
-              />
-              <h2 className="bold-text" style={{ marginTop: 20 }}>
-                {about.title}
-              </h2>
-              <p className="p-text2" style={{ marginTop: 10 }}>
-                {about.description}
-              </p>
-            </motion.div>
-          ))}
+          {abouts.map((about, index) => {
+            const key = about.title + index;
+            const isHovered = hovered === key;
+            return (
+              <motion.div
+                whileInView={{ opacity: 1 }}
+                whileHover={{ scale: 1.1 }}
+                transition={{ duration: 0.5, type: "tween" }}
+                className={`app__profile-item box__container ${isHovered && "hovered"}`}
+                key={key}
+                onMouseEnter={() => handleMouseEnter(key)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <img
+                  src={urlFor(about.imgUrl)}
+                  alt={about.title}
+                  className="shadow__1"
+                />
+                <h2 className="bold-text" style={{ marginTop: 20 }}>
+                  {about.title}
+                </h2>
+                <p className="p-text2" style={{ marginTop: 10 }}>
+                  {about.description}
+                </p>
+              </motion.div>
+            );
+          })}
         </div>
       </div>
     </>
